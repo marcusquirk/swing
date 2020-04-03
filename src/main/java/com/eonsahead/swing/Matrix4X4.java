90badb5ed16b28b4957a50c549a0aa5d13e9997b
marcusquirk <marcus@192.168.0.19>
2020-04-03 09:42:22 -0500

    Added javadoc comments for the Matrix4X4 class.

--- src/main/java/com/eonsahead/swing/Matrix4X4.java
+++ src/main/java/com/eonsahead/swing/Matrix4X4.java
@@ -13,11 +13,17 @@ public final class Matrix4X4 {
 
     private final double[][] elements;
 
+    /**
+     * The constructor creates a 4X4 identity matrix
+     */
     public Matrix4X4() {
         this.elements = new double[4][4];
         this.identity();
     }
 
+    /**
+     * The method sets this matrix to the identity matrix.
+     */
     public final void identity() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -31,14 +37,33 @@ public final class Matrix4X4 {
         }//for i
     }//identity
 
+    /**
+     * Sets an element in a 4X4 matrix to a given value
+     * @param row The row of the element
+     * @param column The column of the element
+     * @param value The value to set.
+     */
     public void set(int row, int column, double value) {
         this.elements[row][column] = value;
     }
 
+    /**
+     * Returns the value in a certain element.
+     * 
+     * @param row The row of the element
+     * @param column The column of the element
+     * @return The value in the element
+     */
     public double get(int row, int column) {
         return this.elements[row][column];
     }
 
+    /**
+     * Returns a row in string form
+     * 
+     * @row The row to convert to string
+     * @return The row in string form.
+     */
     private String rowToString(int row) {
         StringBuilder result = new StringBuilder();
         result.append("( ");
@@ -51,6 +76,11 @@ public final class Matrix4X4 {
         return result.toString();
     } // rowToString( int )
 
+    /**
+     * Returns the matrix in string form
+     * 
+     * @return The string form of the matrix
+     */
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
@@ -62,6 +92,12 @@ public final class Matrix4X4 {
         return result.toString();
     } // toString()
 
+    /**
+     * Multiplies two matrices together.
+     * 
+     * @param otherMatrix The other matrix to multiply with
+     * @return The product matrix.
+     */
     public Matrix4X4 multiply(Matrix4X4 otherMatrix) {
         Matrix4X4 product = new Matrix4X4();
         for (int i = 0; i < 4; i++) {
@@ -76,6 +112,12 @@ public final class Matrix4X4 {
         return product;
     }
 
+    /**
+     * Multiplies the matrix with a vector.
+     * 
+     * @param vector The vector to multiply with
+     * @return The product vector.
+     */
     public Vector multiply (Vector vector) {
         double[] vectorValues = new double[4];
         for (int i = 0; i < 4; i++) {
@@ -109,6 +151,11 @@ public final class Matrix4X4 {
         System.out.println("net rotation = " + netRotation);
     } // main( String [] )
 
+    /**
+     * Rotates the matrix about the z axis by a given angle.
+     * 
+     * @param angle The angle to rotate by.
+     */
     public void rotationZ(double angle) {
         this.identity();
         this.set(0, 0, Math.cos(angle));
@@ -117,6 +164,11 @@ public final class Matrix4X4 {
         this.set(1, 1, Math.cos(angle));
     } // rotationZ( double )
     
+    /**
+     * Rotates the matrix about the x axis by a given angle.
+     * 
+     * @param angle The angle to rotate by.
+     */
     public void rotationX(double angle){
         this.identity();
         this.set(1, 1, Math.cos(angle));
@@ -125,7 +177,11 @@ public final class Matrix4X4 {
         this.set(2, 2, Math.cos(angle));
     }
     
-    
+    /**
+     * Rotates the matrix about the y axis by a given angle.
+     * 
+     * @param angle The angle to rotate by.
+     */
     public void rotationY(double angle){
         this.identity();
         this.set(0, 0, Math.cos(angle));
@@ -134,6 +190,13 @@ public final class Matrix4X4 {
         this.set(2, 2, Math.cos(angle));
     }
     
+    /**
+     * Turns the matrix into a scaling matrix.
+     * 
+     * @param factorX The x direction scale factor.
+     * @param factorY The y direction scale factor.
+     * @param factorZ The z direction scale factor.
+     */
     public void scale(double factorX, double factorY, double factorZ){
         this.identity();
         this.set(0, 0, factorX);
